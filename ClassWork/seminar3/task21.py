e3b095bd774ec9116098e776acac45b745e67768
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-04-16 18:27:37 +0400

    task21 from seminar 3 solved

---
+++ ClassWork/seminar3/task21.py
@@ -0,0 +1,8 @@
+
+#21. Напишите программу для печати всех уникальных значений в словаре.
+list_1 = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": "S005"}, {" V ":"S009"}, {" VIII ":"S007"}]
+unique_values = set()
+for dicts in list_1:
+    for value in dicts.values():
+        unique_values.add(value)
+print(unique_values)
